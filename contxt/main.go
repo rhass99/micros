c92faad90fb3957672302c50eb2f76b4cb9cee96
rhass99 <rhass99@gmail.com>
2017-11-27 14:56:25 +0400

    added fransesc

---
+++ contxt/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"os"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+
+	go func() {
+		s := bufio.NewScanner(os.Stdin)
+		s.Scan()
+		cancel()
+	}()
+}
